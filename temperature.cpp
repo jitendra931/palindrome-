890a79a58be80597ef4bc1b9ebf838e738fd5f38
jitendra931 <jitendraa7389@gmail.com>
2025-03-09 21:49:38 +0530

    conversion of temperature

--- temperature.cpp
+++ temperature.cpp
@@ -1,17 +1,37 @@
 // convert celcius to  Fahrenheit
 
+// #include<iostream>
+// using namespace std;
+
+// double convertcelcius(double celcius){
+//     return (celcius*9/5) + 32;
+// }
+
+// int main(){
+//     double celcius;
+//     cout<<"enter temperature in celcius : ";
+//     cin>>celcius;
+
+//     cout<<"temperature in fehrenheit : "<<convertcelcius(celcius);
+//     return 0;
+// }
+
+
+// convert Fahrenheit to celcius
+
 #include<iostream>
 using namespace std;
 
-double convertcelcius(double celcius){
-    return (celcius*9/5) + 32;
+double convertfehrenheit(double fehrenheit){
+    return (fehrenheit - 32)*5/9;
 }
 
 int main(){
-    double celcius;
-    cout<<"enter temperature in celcius : ";
-    cin>>celcius;
+    double fehrenheit;
+    cout<<"enter temperature in fehrenheit : ";
+    cin>>fehrenheit;
 
-    cout<<"temperature in fehrenheit : "<<convertcelcius(celcius);
+    cout<<"temperature in celcius : "<<convertfehrenheit(fehrenheit);
+
     return 0;
 }
