48d5d63475ef39e6d8492eaf133a95df2492eac6
jitendra931 <jitendraa7389@gmail.com>
2025-03-09 13:52:21 +0530

    add palindrome number

---
+++ palindrome.cpp
@@ -0,0 +1,24 @@
+// palindrome numbers
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, reversed = 0, original, remainder;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    original = num;
+    while (num != 0) {
+        remainder = num % 10;
+        reversed = reversed * 10 + remainder;
+        num /= 10;
+    }
+
+    if (original == reversed) {
+        cout << original << " is a palindrome." << endl;
+    } else {
+        cout << original << " is not a palindrome." << endl;
+    }
+    return 0;
+}
